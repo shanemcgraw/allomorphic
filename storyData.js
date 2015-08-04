ab5309ff5cdf832831b025c72a99c215fcc6cf6d
Wayne McGrew <shanemcgraw7@gmail.com>
2015-08-04 14:11:17 -0700

    Update everything to include SM user

---
+++ storyData.js
@@ -0,0 +1,163 @@
+var eight = [ 
+["The Celebrated Jumping Frog of Calaveras County", "Mark Twain", 1865],
+["The Rocking-Horse Winner", "D.H. Lawrence", 1926],
+["A Good Man Is Hard to Find", "Flannery O’Connor", 1953]
+];
+
+var seven = [
+["The Necklace", "Guy de Maupassant", 1884],
+["The Lottery", "Shirley Jackson", 1948]
+];
+
+var six = [
+["The Cask of Amontillado", "Edgar Allan Poe", 1846],
+["The Monkey’s Paw", "W. W. Jacobs", 1902],
+["A Hunger Artist", "Franz Kafka", 1922]
+];
+
+var five = [
+["Young Goodman Brown", "Nathaniel Hawthorne", 1835],
+["The Fall of the House of Usher", "Edgar Allan Poe", 1839],
+["The Tell-Tale Heart", "Edgar Allan Poe", 1843],
+["An Occurrence at Owl Creek Bridge", "Ambrose Bierce", 1890],
+["The Yellow Wallpaper", "Charlotte Perkins Gilman", 1892],
+["To Build a Fire", "Jack London", 1908],
+["The Dead", "James Joyce", 1914],
+["In the Penal Colony", "Franz Kafka", 1914, pub. 1919],
+["The Garden Party", "Katherine Mansfield", 1922],
+["Hills Like White Elephants", "Ernest Hemingway", 1927],
+["Harrison Bergeron", "Kurt Vonnegut, Jr.", 1961]
+];
+
+var four = [
+["The Legend of Sleepy Hollow", "Washington Irving", 1820],
+["The Minister’s Black Veil", "Nathaniel Hawthorne", 1836],
+["The Masque of the Red Death", "Edgar Allan Poe", 1842],
+["The Purloined Letter", "Edgar Allan Poe", 1844],
+["Bartleby the Scrivener", "Herman Melville", 1853],
+["The Luck of Roaring Camp", "Bret Harte", 1868],
+["The Outcasts of Poker Flat", "Bret Harte", 1869],
+["Rikki-Tikki-Tavi", "Rudyard Kipling", 1894],
+["Regret", "Kate Chopin", 1894],
+["The Story of An Hour", "Kate Chopin", 1894],
+["The Lady With The Dog", "Anton Chekhov", 1899],
+["How the Leopard Got His Spots", "Rudyard Kipling", 1902],
+["Araby", "James Joyce", 1914],
+["Goodbye", My Brother, by John Cheever", 1951],
+["The Swimmer", "John Cheever", 1964],
+["Cathedral", "Raymond Carver", 1983]
+];
+var three = [
+["A Passion in the Desert", "Honoré de Balzac", 1830],
+["The Pit and the Pendulum", "Edgar Allan Poe", 1842],
+["The Birth-Mark", "Nathaniel Hawthorne", 1843],
+["Thrawn Janet", "Robert Louis Stevenson ", 1881],
+["The Death of Ivan Ilych", "Leo Tolstoy", 1886],
+["The Open Boat", "Stephen Crane", 1897],
+["Gooseberries", "Anton Chekhov", 1898],
+["A Dark Brown Dog", "Stephen Crane", 1901],
+["The Gift of the Magi", "O. Henry", 1905],
+["Eveline", "James Joyce", 1914],
+["Bliss", "Katherine Mansfield", 1920],
+["The Fly", "Katherine Mansfield", 1922],
+["The Diamond As Big as the Ritz", "F. Scott Fitzgerald", 1922],
+["The Most Dangerous Game", "Richard Connell", 1924],
+["The Story of My Dovecot", "Isaac Babel", 1925],
+["A Jury of Her Peers", "Susan Glaspell", 1927],
+["The Killers", "Ernest Hemingway", 1927],
+["A Rose for Emily", "William Faulkner", 1930],
+["That Evening Sun", "William Faulkner", 1931],
+["Guests of the Nation", "Frank O’Connor", 1931],
+["A Clean, Well-Lighted Place", "Ernest Hemingway", 1933],
+["The Devil and Daniel Webster", "Stephen Vincent Benét", 1937],
+["The Use of Force", "William Carlos Williams", 1938],
+["The Door", "E. B White", 1939],
+["A Very Old Man with Enormous Wings", "Gabriel García Márquez", 1955],
+["Sonny’s Blues", "James Baldwin", 1957],
+["A & P", "John Updike", 1961],
+["Where Are You Going, Where Have You Been?", "Joyce Carol Oates", 1966]
+];
+var two = [
+["The Ambitious Guest", "Nathaniel Hawthorne", 1835],
+["The Diary of a Madman", "Nikolai Gogol", 1835],
+["The Nose", "Nikolai Gogol", 1835-1836],
+["The Eclipse", "James Fenimore Cooper", 1833-1838],
+["The Baron of Grogzwig", "Charles Dickens", 1839],
+["The Overcoat", "Nikolai Gogol", 1842],
+["A Christmas Carol", "Charles Dickens", 1843],
+["The Lightning-Rod Man", "Herman Melville", 1854],
+["Scarlet Stockings", "Louisa May Alcott", 1869],
+["Boule de Suif", "Guy de Maupassant", 1880],
+["A Lodging for the Night", "Robert Louis Stevenson", 1882],
+["Two Views of The Mississippi", "Mark Twain", 1883],
+["The Looking Glass", "Anton Chekhov", 1885],
+["The Private History of a Campaign That Failed", "Mark Twain", 1885],
+["The Griffin and the Minor Canon", "Frank Stockton", 1885],
+["The Strange Case of Dr Jekyll and Mr Hyde", "Robert Louis Stevenson", 1886],
+["The Nightingale and the Rose", "Oscar Wilde", 1888],
+["The Man Who Would Be King", "Rudyard Kipling", 1888],
+["A Horseman in the Sky", "Ambrose Bierce", 1889],
+["The Boarded Window", "Ambrose Bierce", 1891],
+["Under The Lion’s Paw", "Hamlin Garland", 1891],
+["Desiree’s Baby", "Kate Chopin", 1893],
+["Little Herr Friedemann", "Thomas Mann", 1896],
+["Heart of Darkness", "Joseph Conrad", 1899],
+["Youth", "Joseph Conrad", 1902],
+["Was it Heaven? Or Hell?", "Mark Twain", 1903],
+["Eve’s Diary", "Mark Twain", 1905],
+["Paul’s Case", "Willa Cather", 1905],
+["The Skylight Room", "O. Henry", 1906],
+["The Last Leaf", "O. Henry", 1906],
+["On The Gull’s Road", "Willa Cather", 1908],
+["The Cactus", "O. Henry", c. 1905-1909],
+["The Princess And The Puma", "O. Henry", c. 1905],
+["A Municipal Report", "O. Henry", 1905],
+["The Secret Sharer", "Joseph Conrad", 1909],
+["Sredni Vashtar", "Saki (Hugh Munro)", 1900],
+["The Prussian Officer", D H Lawrence", 1914],
+["The Metamorphosis", "Franz Kafka", 1915],
+["The Tale", "Joseph Conrad", 1917],
+["A. V. Laider", "Max Beerbohm", 1919],
+["Kew Gardens", "Virginia Woolf", 1919],
+["A Haunted House", "Virginia Woolf", 1921],
+["Rain", "W. Somerset Maugham", 1921],
+["Haircut", "Ring Lardner", 1925],
+["My First Goose", "Isaac Babel", 1926],
+["A Telephone Call", "Dorothy Parker", 1930],
+["Theft", "Katherine Anne Porter", 1930],
+["Dry September", "William Faulkner", 1931],
+["Guy de Maupassant", "Isaac Babel", 1932],
+["Death In The Woods", "Sherwood Anderson", 1933],
+["Shooting An Elephant", "George Orwell", 1936],
+["Spring in Fialta", "Vladimir Nabokov", 1936],
+["In Dreams Begin Responsibilities", "Delmore Schwartz", 1937],
+["Leiningen versus the Ants", "Carl Stephenson", 1938],
+["The Girls In Their Summer Dresses", "Irwin Shaw", 1939],
+["The Wall", "Jean-Paul Sartre", 1939],
+["Tlon, Uqbar, Orbis Tertius", "Jorge Luis Borges", 1940],
+["Madame Zilensky and the King of Finland", "Carson McCullers", 1941],
+["Beware of The Dog", "Roald Dahl", 1944],
+["The Aleph", "Jorge Luis Borges", 1945],
+["For Esmé – with Love and Squalor", "JD Salinger", 1950],
+["The Chaser", "John Collier", 1951],
+["A Sound of Thunder", "Ray Bradbury", 1952],
+["A Mother’s Tale", "James Agee", 1952],
+["The Five-Forty-Eight", "John Cheever", 1954],
+["The Ledge", "Lawrence Sargent Hall", 1959]
+];
+
+
+var storyData = [eight, seven, six, five, four, three, two];
+
+
+function formTextOb(textArray){
+	this.title = textArray[0];
+	this.author = textArray[1];
+	this.dateWritten = textArray[2];
+}
+
+
+
+
+
+
